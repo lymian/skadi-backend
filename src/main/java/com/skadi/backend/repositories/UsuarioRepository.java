6de62f6d46128dec360552fb4868eed56a0f0b2f
mianlol01 <snake.1503@outlook.com>
2026-01-08 03:27:41 -0500

    s

--- src/main/java/com/skadi/backend/repositories/UsuarioRepository.java
+++ src/main/java/com/skadi/backend/repositories/UsuarioRepository.java
@@ -2,6 +2,8 @@ package com.skadi.backend.repositories;
 
 import com.skadi.backend.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +12,8 @@ import java.util.Optional;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    Optional<Usuario> findByUsername(String username);
+    @Query("SELECT u FROM Usuario u JOIN FETCH u.empresa WHERE u.username = :username")
+    Optional<Usuario> findByUsername(@Param("username") String username);
 
     List<Usuario> findByEmpresaId(Long empresaId);
 
